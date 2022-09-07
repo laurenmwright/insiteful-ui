41ab9835dc825a53f39237916bbf1c02eebf484b
tmdawson23 <78229709+tmdawson23@users.noreply.github.com>
2022-09-07 16:09:49 -0500

    Caption Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import './lib/components/App.css';
+import "./lib/components/App.css";
 import React, { useState, useEffect, useContext } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Button, ThemeContext, Heading1 } from "./lib";
-import Theme from './lib/components/Theme';
-
+import { Caption1, Caption2 } from "./lib/components/Typography";
+import Theme from "./lib/components/Theme";
 
 const colorPaletteTest = {
-  "primary": "#FF0000",
-  "dark_gray": "#333333",
-  "secondary": "#E0C591",
-  "light_gray": "#797B7A",
-  "primary_shade_1": "#46575E",
-  "primary_shade_2": "#748186",
-  "primary_shade_3": "#A3ABAF",
-  "secondary_shade_1": "#ECDCBD",
-  "secondary_shade_2": "#F6EEDE",
-  "secondary_shade_3": "#F9F3E9"
-}; 
-
+  primary: "#FF0000",
+  dark_gray: "#333333",
+  secondary: "#E0C591",
+  light_gray: "#797B7A",
+  primary_shade_1: "#46575E",
+  primary_shade_2: "#748186",
+  primary_shade_3: "#A3ABAF",
+  secondary_shade_1: "#ECDCBD",
+  secondary_shade_2: "#F6EEDE",
+  secondary_shade_3: "#F9F3E9",
+};
 
 function App() {
-
   const { colorPalette, changeColorPalette } = useContext(ThemeContext);
 
   useEffect(() => {
@@ -33,36 +27,31 @@ function App() {
 
   return (
     <Router>
-       {/* <NavBar name="Lauren Wright" fontColor="#2E2E2E" backgroundColor="#CCCCFF"  links={routeData}/>  */}
-        <Routes>
-        <Route path="/"  element={<Home />}>
-          </Route>
-          <Route path="/about" element={<About />}>
-          </Route>
-          <Route path="/blogs" element={<Blogs />}>
-          </Route>
-        </Routes>
+      {/* <NavBar name="Lauren Wright" fontColor="#2E2E2E" backgroundColor="#CCCCFF"  links={routeData}/>  */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+      </Routes>
     </Router>
   );
 }
 
 function Home() {
   return (
-    <div >
-      <Button primary overrideColor="#ff0000"> HI</Button>
+    <div>
+      <Button primary overrideColor="#ff0000">HI</Button>
       <Heading1>HelloWorld</Heading1>
+      <Caption1 overrideColor = "red" overrideFont = "Times New Roman">Caption1</Caption1>
+      <Caption2 overrideColor = "blue" overrideFont = "Calabri">Caption2</Caption2>
     </div>
-  )
+  );
 }
 function About() {
-  return (
-    <div>About</div>
-  )
+  return <div>About</div>;
 }
 function Blogs() {
-  return (
-    <div>Its different</div>
-  )
+  return <div>Its different</div>;
 }
 
 export default App;
--- src/__test__/Typography.spec.tsx
+++ src/__test__/Typography.spec.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import {Heading1} from '../lib/components/Typography/Typography';
+import {Heading1, Caption1, Caption2} from '../lib/components/Typography/Typography';
 
-//Heading1
+// Heading1
 // test for children(text)
 test('renders button with correct text', () => {
   render(<Heading1>Heading1</Heading1>);
   const buttonElement = screen.getByText(/Heading1/i);
   expect(buttonElement).toBeInTheDocument();
 });
+
 //Heading1
 // test for override-color 
 // test('renders button with correct color', () => {
@@ -16,6 +17,7 @@ test('renders button with correct text', () => {
 //   const buttonElement = screen.getByText(/My Button/i);
 //   expect(buttonElement).toHaveStyle({backgroundColor:"#ff0000"});
 // });
+
 //Heading1
 // test for override-font
 // test('renders button with correct font', () => {
@@ -26,3 +28,53 @@ test('renders button with correct text', () => {
 //   fireEvent.click(getByText(/My Button/i));
 //   expect(hello).toHaveBeenCalled();
 // });
+
+//Caption1
+//test for children(text)
+test("renders caption with correct text", () => {
+  render(<Caption1>Caption1</Caption1>);
+  const captionElement = screen.getByText(/Caption1/i);
+  expect(captionElement).toBeInTheDocument();
+});
+
+//Caption1
+// test for override-color 
+test('renders caption with correct color', () => {
+  render(<Caption1 overrideColor = "red" >Caption1</Caption1>);
+  const captionElement = screen.getByText(/Caption1/i);
+  expect(captionElement).toHaveStyle({color:"red"});
+});
+
+//Caption1
+// test for override-font
+test("renders caption with correct color", () => {
+  render(<Caption1 overrideFont="Arial">Caption1</Caption1>);
+  const captionElement = screen.getByText(/Caption1/i);
+  expect(captionElement).toHaveStyle({ fontFamily: "Arial" });
+});
+
+//Caption2
+//test for children(text)
+test("renders caption with correct text", () => {
+  render(<Caption2>Caption2</Caption2>);
+  const caption2Element = screen.getByText(/Caption2/i);
+  expect(caption2Element).toBeInTheDocument();
+});
+
+//Caption2
+// test for override-color 
+test('renders caption with correct color', () => {
+  render(<Caption2 overrideColor = "red" >Caption2</Caption2>);
+  const caption2Element = screen.getByText(/Caption2/i);
+  expect(caption2Element).toHaveStyle({color:"red"});
+});
+
+//Caption2
+// test for override-font
+test("renders caption with correct color", () => {
+  render(<Caption2 overrideFont="Arial">Caption2</Caption2>);
+  const caption2Element = screen.getByText(/Caption2/i);
+  expect(caption2Element).toHaveStyle({ fontFamily: "Arial" });
+});
+
+
