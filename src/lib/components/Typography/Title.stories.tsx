f543f4d5a50f3a5da69652a21cdf8805379a1e2c
Peyton Reed <pdreed@crimson.ua.edu>
2022-09-07 16:02:08 -0500

    a few storybook examples

---
+++ src/lib/components/Typography/Title.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions'
+
+import {Title, Heading1} from './Typography';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Title',
+  component: Title,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+//    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Title>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Title> = (args) => <Title {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  children: "Title",
+};
+
