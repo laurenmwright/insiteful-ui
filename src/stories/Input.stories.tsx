bb74e1e4c289250e509af1541b27361b248edc8f
tmdawson23 <78229709+tmdawson23@users.noreply.github.com>
2022-09-20 12:02:33 -0500

    Input

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { action } from "@storybook/addon-actions"
+import { Input } from "../lib/components/Input/Input";
+
+export default {
+  title: 'Stories/Input',
+  component: Input,
+  argTypes: {
+    onChanged: {action: 'changed '}
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  primary: true,
+
+  onChange: action("Input is changed!")
+};
