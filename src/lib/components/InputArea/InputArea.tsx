59ce59fbb5c7daab0978430408b999affb17b7e0
tmdawson23 <78229709+tmdawson23@users.noreply.github.com>
2022-09-21 14:16:27 -0500

    Removed width from tsx

--- src/lib/components/InputArea/InputArea.tsx
+++ src/lib/components/InputArea/InputArea.tsx
@@ -29,7 +29,6 @@ export const InputArea = ({
       <input
         value={value}
         type={type}
-        style = {{width : 200}}
         className={'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder!}
