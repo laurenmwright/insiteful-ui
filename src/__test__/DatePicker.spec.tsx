1b211a6076573c9376b6c6d8b346a26469e107c6
CreightonAlexHolt <caholt2@crimson.ua.edu>
2022-10-05 16:03:32 -0500

    Final

--- src/__test__/DatePicker.spec.tsx
+++ src/__test__/DatePicker.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { fireEvent, getByDisplayValue, getByRole, getByTestId, render, screen} from '@testing-library/react';
 import { DatePicker } from '../components';
+import userEvent from "@testing-library/user-event";
 
 const myTest = () => {
     console.log("test");
@@ -9,4 +10,13 @@ test('renders DRP', () => {
     render(<DatePicker onChange={myTest} rangeValue="1999-01-30"></DatePicker>);
     const DateRangeElement = screen.getByTestId("DRP");
     expect(DateRangeElement).toHaveValue("1999-01-30");
+});
+
+test("on change gets called", () => {
+  const hello = jest.fn();
+  const {getByTestId} = render(<DatePicker onChange={hello}/>);
+  const DateRangeElement = getByTestId("DRP");
+  //expect(DateRangeElement).toContainElement(getByTestId("DRP"));
+  userEvent.type(DateRangeElement, "test"); 
+  expect(hello).toHaveBeenCalled;
 });
--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -3,17 +3,22 @@ import React from "react";
 export type DatePickerProps = {
     rangeValue?: string;
     onChange: (newValue: string) => void;
+    min?: any;
+    max?: any;
 }
 
 export const DatePicker = ({
     rangeValue, 
     onChange,
+    min,
+    max,
+
 }: DatePickerProps) =>{
     return(
         <input type ="date" 
             data-testid="DRP" 
-            value={rangeValue}
             onChange={(e) => onChange(e.target.value)}
+            className={'shadow appearance-none border rounded w-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
         />
     )
 }
