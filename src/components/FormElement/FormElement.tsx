e0a95b7be9e491cb825d1af7146b733211eb70ac
Peyton Reed <pdreed@crimson.ua.edu>
2022-10-05 13:22:32 -0500

    Form element styling

--- src/components/FormElement/FormElement.tsx
+++ src/components/FormElement/FormElement.tsx
@@ -5,17 +5,15 @@ import classNames from "classnames";
 export type FormElementProps = {
   label?: string;
   children?: React.ReactNode | React.ReactNode[];
-  twClasses?: string;
 };
 
 export function FormElement({
   label,
-  twClasses,
   children,
 }: FormElementProps) {
   return (
-    <div data-testid="form" className={ "mb-6 w-40" + (twClasses ?  " " + twClasses: "" )}>
-      <label data-testid="label" >{label}</label>
+    <div data-testid="form" className={classNames(styles.formElement)}>
+      <label data-testid="label" className={classNames(styles.label)}>{label}</label>
       {children}
     </div>
   );
