e90ad128e806790f56366a478412a326c506cfd7
Peyton Reed <pdreed@crimson.ua.edu>
2022-10-05 17:17:54 -0500

    date picker changes

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 export type DatePickerProps = {
-    rangeValue?: string;
+    value?: string;
     onChange: (newValue: string) => void;
 }
 
 export const DatePicker = ({
-    rangeValue, 
+    value, 
     onChange,
 }: DatePickerProps) =>{
     return(
         <input type ="date" 
             data-testid="DRP" 
-            value={rangeValue}
+            value={value}
             onChange={(e) => onChange(e.target.value)}
+            className={'shadow appearance-none border rounded w-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
         />
     )
 }
