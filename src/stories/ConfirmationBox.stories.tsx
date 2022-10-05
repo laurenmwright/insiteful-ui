74dfd39e0974da3d2ccc676ad91d0854fabc103a
thomasmccroan50 <tmmccroan@crimson.ua.edu>
2022-10-05 16:30:07 -0500

    Storybook test updated

--- src/stories/ConfirmationBox.stories.tsx
+++ src/stories/ConfirmationBox.stories.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ConfirmationBox } from "../components";
+import { action } from '@storybook/addon-actions'
+
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Stories/Components/ConfirmationBox',
   component: ConfirmationBox,
+  subcomponents: {ConfirmationBox},
   parameters: {
     layout: 'centered',
+  },
+  argTypes: {
+    onCancel: { action: 'clicked' },
+    onConfirm: { action: 'clicked' },
   },
 } as ComponentMeta<typeof ConfirmationBox>;
 
 const Template: ComponentStory<typeof ConfirmationBox> = (args) => <ConfirmationBox {...args} />;
 
-const myConfirm = () => {
-    console.log("USER CONFIRMED");
-  }
-const myCancel = () => {
-    console.log("USER CANCELLED");
-  }
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+  Primary.args = {
     popButton: "Click me",
     panelText: "You sure?????",
-    onConfirm: myConfirm,
-    onCancel: myCancel,
+    onConfirm:action("user confirmed!"),
+    onCancel:action("user canceled!"),
 };
