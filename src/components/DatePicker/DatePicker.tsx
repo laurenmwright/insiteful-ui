0fa12b218274ccf1c386eae67a680378d53cd923
CreightonAlexHolt <caholt2@crimson.ua.edu>
2022-10-06 08:12:29 -0500

    Copy this

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -18,7 +18,7 @@ export const DatePicker = ({
         <input type ="date" 
             data-testid="DRP" 
             onChange={(e) => onChange(e.target.value)}
-            className={'shadow appearance-none border rounded w-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
+            className={'shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
         />
     )
 }
