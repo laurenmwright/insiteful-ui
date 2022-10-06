6af5735b57d93d47d8affdfb4724cace65a2606b
CreightonAlexHolt <caholt2@crimson.ua.edu>
2022-10-06 08:21:41 -0500

    Final I think

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -3,18 +3,24 @@ import React from "react";
 export type DatePickerProps = {
     rangeValue?: string;
     onChange: (newValue: string) => void;
+    min?: any;
+    max?: any;
 }
 
 export const DatePicker = ({
     rangeValue, 
     onChange,
+    min,
+    max,
+
 }: DatePickerProps) =>{
     return(
         <input type ="date" 
             data-testid="DRP" 
-            value={rangeValue}
             onChange={(e) => onChange(e.target.value)}
+            className={'shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
         />
     )
 }
 export default DatePicker;
+
