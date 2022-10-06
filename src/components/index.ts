7f127191653b7ce546b7f634f973b4ea44114c96
Peyton Reed <pdreed@crimson.ua.edu>
2022-10-06 08:41:32 -0500

    removing unnecessary import

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,7 +10,6 @@ export { FormElement } from "./FormElement";
 export { Accordion } from "./Accordion";
 export { Modal } from "./Modal";
 export { DatePicker } from "./DatePicker";
-export { ConfirmationBox } from "./ConfirmationBox";
 export { Tabs } from "./Tabs";
 export { default as ThemeProvider } from "./Theme";
 export { ThemeContext } from "./Theme";
