578e49137f1ea8243b7095dcd7e60ee1dd9db7cc
Garrett Dickerson <gsdickerson@crimson.ua.edu>
2022-10-10 12:46:52 -0500

    Completed Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,37 +1,70 @@
 import { Dialog } from "@headlessui/react";
 import classNames from "classnames";
 import { X } from 'react-feather';
+import Button from "../Button";
+import { FormElement } from "../FormElement";
+import { Toggle } from "../Toggle";
 import { Title } from "../Typography";
 import styles from './Modal.module.css';
+
+const small = 25;
+const medium = 50;
+const large = 75;
 
 export type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
   title?: string;
   children?: React.ReactNode | React.ReactNode[];
+  // size: typeof small | typeof medium | typeof large;
+  size: "small" | "medium" | "large";
+  // size: number;
+  
 };
 
-export const Modal = ({ isOpen, onClose, title, children }: ModalProps) => {
+
+
+
+
+export const Modal = ({ isOpen, onClose, title, children, size}: ModalProps) => {
+  //Set 
+  var root = document.documentElement;
+  root.style.setProperty('--width', size);
+
+
   return (
-  <div className={ classNames(styles.dialogBox)}>
-    <Dialog data-testid="dialog" as="div" className="relative z-10" open={isOpen} onClose={onClose}>
-      <div className="fixed inset-0 overflow-y-auto flex min-h-full items-center justify-center p-4 text-center">
-          <Dialog.Panel data-testid="panel"  className={ classNames(styles.dialogBox) }>
-            <span className={classNames(styles.button)}>
-            <button data-testid="icon" onClick = {onClose}>
+      <div>
+
+      {/* <Dialog data-testid="dialog" as="div" open={isOpen} onClose={onClose} className={ classNames(styles.dialogMedium)}> */}
+      {isOpen && <Dialog data-testid="dialog" as="div" open={isOpen} onClose={onClose} className={ (size==='small' ? classNames(styles.dialogSmall) : size==='medium' ? classNames(styles.dialogMedium) : size==='large' ? classNames(styles.dialogLarge) : classNames(styles.dialogMedium)) }>
+        <div>
+
+          <Dialog.Panel data-testid="panel" >
+            {/* Class name in 'button' right aligns the X-button */}
+            <button data-testid="icon" onClick = {onClose} className={ classNames(styles.buttonIcon)} >
               <X color='red' size={18} />
             </button>
-            </span>
+
+            {/* <div className={ classNames(styles.header) }> */}
             {title && (
-              <span className={classNames(styles.header)}>
-                <Dialog.Title data-testid="title"> 
+                <Dialog.Title data-testid="title" className={ classNames(styles.title)}> 
                   <Title>{title}</Title>
                 </Dialog.Title>
-              </span>
+                
             )}
-            <div className={classNames(styles.modalChildren)}>{children}</div>
+            {/* </div> */}
+
+            {/* {size} */}
+            {children}
+            <br></br>
+            <Button primary>Button</Button>
+            <br></br>
+            <FormElement>Form Element</FormElement>
+            <br></br>
+            <Button secondary>Button 2</Button>
           </Dialog.Panel>
-      </div>
-    </Dialog>
-  </div>
+        </div>    
+      </Dialog> 
+      }
+    </div>
   )}
