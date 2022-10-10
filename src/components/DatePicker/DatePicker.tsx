884b1f712c4c18be9a0706a9cf89fa83d7b551b4
CreightonAlexHolt <caholt2@crimson.ua.edu>
2022-10-10 12:24:05 -0500

    Please be done

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,27 +1,25 @@
 import React from "react";
-
-import styles from "./DatePicker.module.css";
-
+import styles from './DatePicker.module.css';
 import classNames from "classnames";
 
 export type DatePickerProps = {
     dateValue?: string;
-    onChange?: (newValue: string) => void;
+    onChange: (newValue: string) => void;
+
 }
 
 export const DatePicker = ({
     dateValue, 
     onChange,
-    min,
-    max,
 
 }: DatePickerProps) =>{
     return(
         <input type ="date" 
             data-testid="DRP"
-            value={dateValue}
-            onChange={(e) => onChange!(e.target.value)}
-            className={'shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
+            value = {dateValue} 
+            className={classNames(styles.input)}
+            onChange={(e) => onChange(e.target.value)}
+           
         />
     )
 }
--- src/stories/DatePicker.stories.tsx
+++ src/stories/DatePicker.stories.tsx
@@ -3,21 +3,38 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { action } from "@storybook/addon-actions";
 import { DatePicker } from '../components';
 import { string } from 'prop-types';
+import { useArgs } from "@storybook/client-api";
+
 
 export default {
-  title: 'Stories/Components/DatePicker',
+  title: "Stories/Components/DatePicker",
   component: DatePicker,
-  subcomponents: {DatePicker},
   argTypes: {
-    onChange: { action: 'changed' }
+    dateValue: {
+      control: "text"
+    },
   },
+  args: {
+    dateValue: "2020-10-10"
+  },
+
 } as ComponentMeta<typeof DatePicker>;
-
 const Template: ComponentStory<typeof DatePicker> = (args) => <DatePicker {...args} />;
-
-export const Primary = Template.bind({});
-
-Primary.args = {
-  dateValue: string,
-  onChange: action("Date is changed!")
+export const Controlled = (args) => {
+  const [_, setArgs] = useArgs();
+  const onChange = (e, dateValue) => {
+    setArgs({ ...args, dateValue });
+    args.onChange(e, dateValue);
+  };
+  return <Template {...args} onChange={onChange} />;
 };
+
+Controlled.argTypes = {
+  onChange: { action: "clicked", table: { disable: true } },
+};
+
+Controlled.args = {
+  dateValue: "",
+};
+
+export const Uncontrolled = Template.bind({});
