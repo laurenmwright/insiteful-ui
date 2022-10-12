da156b835c2f1e3b2f25d1a92e1c99cdf6b3eea1
lmwright6 <laurenwpersonal@gmail.com>
2022-10-12 09:25:14 -0500

    toggle updated

--- src/__test__/Toggle.spec.tsx
+++ src/__test__/Toggle.spec.tsx
@@ -3,7 +3,7 @@ import { fireEvent, render} from '@testing-library/react';
 import {Toggle} from '../components';
 
 test('renders toggle correctly', () => {
-    const {getByTestId} = render(<Toggle label={"Label"} enabled={true} setEnabled={() => {}}/>);
+    const {getByTestId} = render(<Toggle label={"Label"} enabled={true} onChange={() => {}}/>);
     const toggleElement = getByTestId("div");
     expect(toggleElement).toContainElement(getByTestId("label"));
     expect(toggleElement).toContainElement(getByTestId("switch"));
@@ -11,26 +11,26 @@ test('renders toggle correctly', () => {
 });
 
 test('renders toggle with correct disabled color', () => {
-    const {getByTestId} = render(<Toggle label="Label" EnabledColor="bg-blue-500" DisabledColor = "bg-gray-300" enabled={false} setEnabled={() => {}}/>);
+    const {getByTestId} = render(<Toggle label="Label" enabled={false} onChange={() => {}}/>);
     const toggleElement = getByTestId("switch");
     expect(toggleElement).toHaveClass('bg-gray-300');
 });
 
 test('enable changes toggle color', () => {
-    const {getByTestId} = render(<Toggle label="Label" EnabledColor="bg-blue-500" DisabledColor = "bg-gray-300" enabled={true} setEnabled={() => {}}/>);
+    const {getByTestId} = render(<Toggle label="Label" enabled={true} onChange={() => {}}/>);
     const toggleElement = getByTestId("switch");
     fireEvent.click(toggleElement);
     expect(toggleElement).toHaveClass('bg-blue-500');
 });
 
 test('renders toggle with disabled/secondary color', () => {
-    const {getByTestId} = render(<Toggle label="Label" enabled={false} setEnabled={() => {}}/>);
+    const {getByTestId} = render(<Toggle label="Label" enabled={false} onChange={() => {}}/>);
     const toggleElement = getByTestId("switch");
     expect(toggleElement).toHaveClass('bg-secondary');
 });
 
 test('enable changes toggle to primary color', () => {
-    const {getByTestId} = render(<Toggle label="Label" enabled={true} setEnabled={() => {}}/>);
+    const {getByTestId} = render(<Toggle label="Label" enabled={true} onChange={() => {}}/>);
     const toggleElement = getByTestId("switch");
     fireEvent.click(toggleElement);
     expect(toggleElement).toHaveClass('bg-primary');
@@ -42,7 +42,7 @@ test('changing state when selecting an option', () => {
     const useStateMock: any = (useState: any) => [useState, setStateMock];
     jest.spyOn(React, 'useState').mockImplementation(useStateMock);
 
-    const {getByTestId} = render(<Toggle label={"Label"} enabled = {true} setEnabled = {setStateMock} />);
+    const {getByTestId} = render(<Toggle label={"Label"} enabled = {true} onChange = {setStateMock} />);
     const toggleElement = getByTestId("switch");
     fireEvent.click(toggleElement);
 
