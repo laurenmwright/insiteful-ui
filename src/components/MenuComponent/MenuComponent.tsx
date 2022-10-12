4e4be2230c00b4080ea1a6702dc8faba289a1bba
tmdawson23 <78229709+tmdawson23@users.noreply.github.com>
2022-10-12 14:13:36 -0500

    Changes to MenuComponent

--- src/components/MenuComponent/MenuComponent.tsx
+++ src/components/MenuComponent/MenuComponent.tsx
@@ -14,10 +14,10 @@ export const MenuComponent = ({ data }: MenuProps) => {
     <div className="fixed top-16 w-56 text-right">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-actionBlue bg-opacity-40 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-actionBlue px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             Options
             <ChevronDown
-              className="ml-2 -mr-1 h-5 w-5 text-actionBlue-200 hover:text-actionBlue-100"
+              className="ml-2 -mr-1 h-5 w-5"
               aria-hidden="true"
             />
           </Menu.Button>
@@ -31,14 +31,14 @@ export const MenuComponent = ({ data }: MenuProps) => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
               {data.map((data, index) => (
                 <Menu.Item data-testid={index}>
                   {({ active }) => (
                     <button
                       className={`${
-                        active ? "bg-lightGray text-white" : "text-gray-900"
+                        active ? "bg-lightGray text-darkGray" : "text-darkGray"
                       } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
                       {data.label}
