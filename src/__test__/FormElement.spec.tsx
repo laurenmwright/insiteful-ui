a7cf0264f72797b0141944e0d8fe05fda2d03302
Peyton Reed <pdreed@crimson.ua.edu>
2022-10-12 12:58:41 -0500

    fixing the unit test errors

--- src/__test__/FormElement.spec.tsx
+++ src/__test__/FormElement.spec.tsx
@@ -4,9 +4,9 @@ import { Button } from "../components";
 import { FormElement } from "../components";
 
 test('renders button inside of a form element', () => {
-  const {getByTestId} = render(<FormElement><Button>My Button</Button></FormElement>);
-  const formElement = getByTestId("form");
-  const labelElement = getByTestId("label");
+  const {getByTestId} = render(<FormElement label="Label"><Button>My Button</Button></FormElement>);
+  const formElement = getByTestId("form-element");
+  const labelElement = screen.getByText(/Label/i);
   const buttonElement = screen.getByText(/My Button/i);
   expect(formElement).toBeInTheDocument();
   expect(labelElement).toBeInTheDocument();
