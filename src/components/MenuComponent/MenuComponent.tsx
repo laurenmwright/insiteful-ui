605f546d5da2fe983f4942de64210906884e606b
Peyton Reed <pdreed@crimson.ua.edu>
2022-10-17 12:31:07 -0500

    removing ButtonTyp

--- src/components/MenuComponent/MenuComponent.tsx
+++ src/components/MenuComponent/MenuComponent.tsx
@@ -17,7 +17,7 @@ export const MenuComponent = ({ buttonText, data }: MenuProps) => {
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-actionBlue px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-            <ButtonTypography>{buttonText}</ButtonTypography>
+            {buttonText}
             <ChevronDown className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
           </Menu.Button>
         </div>
