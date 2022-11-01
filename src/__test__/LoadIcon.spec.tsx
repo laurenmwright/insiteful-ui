dbd9cc98ddec48324ca7e62b90fcf77048cc3ee9
Garrett Dickerson <gsdickerson@crimson.ua.edu>
2022-11-01 15:42:38 -0500

    Unit Tests

--- src/__test__/LoadIcon.spec.tsx
+++ src/__test__/LoadIcon.spec.tsx
@@ -7,7 +7,17 @@ import { Load } from "../components";
 
 
 test("renders icon correctly", () => {
-    const {getByTestId} = render(<Load/>);
+    const {getByTestId} = render(<Load />);
     const LoadElement = getByTestId("Feather-Icon-Loader");
     expect(LoadElement).toContainElement(getByTestId("Feather-Icon-Loader"));
+});
+
+test("Correctly applies the centered styling prop", () => {
+    const {getByTestId} = render(<Load center/>)
+    const LoadElement = getByTestId("Feather-Icon-Loader")
+    expect(LoadElement).toHaveStyle({
+        position: "absolute",
+        top: "50%",
+        left: "50%"
+    })
 });
