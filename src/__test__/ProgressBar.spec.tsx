7f50c679f411b7767411a59513d7e7b0f24e5d92
Thomas McCroan <tmmccroan@crimson.ua.edu>
2022-11-02 13:53:18 -0500

    fixed unit test

--- src/__test__/ProgressBar.spec.tsx
+++ src/__test__/ProgressBar.spec.tsx
@@ -3,11 +3,10 @@ import { render } from "@testing-library/react";
 import { ProgressBar } from "../components";
 
 test("renders percentage correctly", () => {
-  const {getByTestId } = render(<ProgressBar percentage={40}></ProgressBar>);
+  const {getByTestId } = render(<ProgressBar percentage={44}></ProgressBar>);
   const progressBarElement = getByTestId("progress-bar");
-  expect(progressBarElement).toHaveTextContent("40%");
+  expect(progressBarElement).toHaveValue(44/100);
 });
-
 
 test("if progress bar background is action blue", () => {
   const {getByTestId} = render(<ProgressBar numerator= {2} denominator= {5}></ProgressBar>);
