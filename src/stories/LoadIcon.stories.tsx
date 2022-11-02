e382949e0fc5a0118d232bbedff7c2333dcd8f56
Peyton Reed <pdreed@crimson.ua.edu>
2022-11-02 12:22:52 -0500

    fixing storybook

--- src/stories/LoadIcon.stories.tsx
+++ src/stories/LoadIcon.stories.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta } from '@storybook/react';
 import { Load } from "../components";
-import { Loader } from 'react-feather';
 
 export default {
   title: 'Stories/Components/LoadIcon',
   component: Load,
   argTypes: {
-    
+    center: { control: { type: 'boolean' } },
+    overrideColor: { control: "text" },
   },
 } as ComponentMeta<typeof Load>;
 
-const Template: ComponentStory<typeof Loader> = (args) => <Loader {...args} />;
-
-// export const Primary = Template.bind({});
 
 export const Primary = (args) => {
     return (
-      <div>
-        <Load />
-      </div>
+      <Load {...args}/>
     );
   };
-
-Primary.args = {
-  center: true
-  
-};
