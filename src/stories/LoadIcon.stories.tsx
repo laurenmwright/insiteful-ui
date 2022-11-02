e0f6995e9c42c9be69e9fa19969251f054ab782a
Garrett Dickerson <gsdickerson@crimson.ua.edu>
2022-11-02 11:46:29 -0500

    Storybook Update

--- src/stories/LoadIcon.stories.tsx
+++ src/stories/LoadIcon.stories.tsx
@@ -7,16 +7,23 @@ export default {
   title: 'Stories/Components/LoadIcon',
   component: Load,
   argTypes: {
-
+    
   },
 } as ComponentMeta<typeof Load>;
 
 const Template: ComponentStory<typeof Loader> = (args) => <Loader {...args} />;
 
-export const Primary = Template.bind({});
+// export const Primary = Template.bind({});
+
+export const Primary = (args) => {
+    return (
+      <div>
+        <Load />
+      </div>
+    );
+  };
 
 Primary.args = {
-
   center: true
-
+  
 };
