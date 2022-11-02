8ed7756d10829329bded205f291305ce08da2e62
Garrett Dickerson <gsdickerson@crimson.ua.edu>
2022-11-02 11:41:12 -0500

    Storybook file

---
+++ src/stories/LoadIcon.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Load } from "../components";
+import { Loader } from 'react-feather';
+
+export default {
+  title: 'Stories/Components/LoadIcon',
+  component: Load,
+  argTypes: {
+
+  },
+} as ComponentMeta<typeof Load>;
+
+const Template: ComponentStory<typeof Loader> = (args) => <Loader {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+
+  center: true
+
+};
