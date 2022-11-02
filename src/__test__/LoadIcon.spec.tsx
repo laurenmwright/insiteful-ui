64ff4c6ccace111429f67ca0689925d1bb9bc471
Peyton Reed <pdreed@crimson.ua.edu>
2022-11-02 12:43:11 -0500

    LoadIcon

---
+++ src/__test__/LoadIcon.spec.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import { Load } from "../components";
+
+test("renders icon correctly", () => {
+    const {getByTestId} = render(<Load />);
+    const LoadElement = getByTestId("Feather-Icon-Loader");
+    expect(LoadElement).toContainElement(getByTestId("Feather-Icon-Loader"));
+});
+
+test("Correctly applies the centered styling prop", () => {
+    const {getByTestId} = render(<Load center/>);
+    const LoadElement = getByTestId("Div-Style");
+    expect(LoadElement).toHaveClass('centerTrue');
+});
+
+test("Correctly applies actionBlue color to loading icon", () => {
+    const {getByTestId} = render(<Load />);
+    const LoadElement = getByTestId("Feather-Icon-Loader");
+    expect(LoadElement).toHaveStyle('color: var(--color-actionBlue)');
+});
